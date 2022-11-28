57419868262d12e23a4678ec90755f7b7af15e3d
Poniac <113772161+Poniac@users.noreply.github.com>
2022-11-28 16:02:27 +0300

    Add files via upload

---
+++ date.js
@@ -0,0 +1,14 @@
+module.exports = getDate;
+
+function getDate () {
+
+  const today = new Date();
+const options = {
+  weekday:"long",
+  day:"numeric",
+  month:"long"
+};
+
+const day = today.toLocaleDateString("en-US", options);
+return day;
+}
